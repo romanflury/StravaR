9f9590a51a0d59ec1e2f3076ecbcaa47cb04d314
roflur <roman.flury@uzh.ch>
2018-12-09 14:59:46 +0100

    some initial commits

---
+++ code/read_gpx.R
@@ -0,0 +1,45 @@
+# read file name and determine activity
+files <- list.files(path = "activities/")
+
+get_activity <- function(file) {
+  dotgpx <- strsplit(file, "-")[[1]][3]
+  activity <- strsplit(dotgpx, ".gpx")[[1]][1]
+
+  return(activity)
+}
+
+## test get_activity
+# table(sapply(files, get_activity))
+#---------------------------------------------------------------------------------- #
+
+setClass("activity",
+         representation(type = "character", name = "character", lat = "numeric", lon = "numeric",
+                        distances = "numeric", elevations = "numeric", times = "POSIXct"),
+         prototype(type = NA_character_, name = NA_character_, lat = NULL, lon = NULL,
+                   distances = NULL, elevations = NULL, times = NULL))
+
+parse_gpx <- function(file) {
+  # read gpx file and decompose XML tree
+  gpxfile <- XML::htmlTreeParse(file, useInternalNodes = TRUE)
+
+  activity_name <- XML::xpathSApply(gpxfile, path = "//name", XML::xmlValue)
+
+  coordinates <- XML::xpathSApply(gpxfile, path = "//trkpt", XML::xmlAttrs)
+  # check that there are at least two coordinates.
+  if (identical(length(coordinates), 0) || ncol(coordinates) < 2) { return(NULL) }
+
+  lat <- as.numeric(coordinates["lat", ])
+  lon <- as.numeric(coordinates["lon", ])
+
+  ele <- as.numeric(XML::xpathSApply(gpxfile, path = "//trkpt/ele", XML::xmlValue))
+  time <- XML::xpathSApply(gpxfile, path = "//trkpt/time", XML::xmlValue)
+
+  activity_dists <- c(0, sp::spDists(x = cbind(lon, lat), longlat = TRUE, segments = TRUE))
+  activity_times <- as.POSIXct(time, tz = "GMT", format = "%Y-%m-%dT%H:%M:%OS")
+
+  result <- new("activity", type = get_activity(file), name = activity_name, lat = lat, lon = lon, distances = activity_dists,
+                elevations = ele, times = activity_times)
+  return(result)
+}
+
+# test <- parse_gpx(file = "activities/20160420-034316-Ride.gpx")
